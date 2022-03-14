d6e13a7581d58201c460285c7582acd3b283c2f6
0xb0x <0xb0x@protonmail.com>
2022-03-14 16:21:43 +0100

    feat: api3-oracle integration

---
+++ contracts/oracle/ISelfServeRrpBeaconServerWhitelister.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <=0.9.0;
+
+interface ISelfServeRrpBeaconServerWhitelister {
+    event SetBeaconIdToExpirationTimestamp(
+        bytes32 indexed beaconId,
+        uint64 expirationTimestamp
+    );
+    event SetBeaconIdToIndefiniteWhitelistStatus(
+        bytes32 indexed beaconId,
+        bool indefiniteWhitelistStatus
+    );
+    event WhitelistedReader(
+        bytes32 indexed beaconId,
+        address indexed reader,
+        uint64 expirationTimestamp,
+        bool indefiniteWhitelistStatus
+    );
+
+    function setBeaconIdToExpirationTimestamp(
+        bytes32 _beaconId,
+        uint64 _expirationTimestamp
+    ) external;
+
+    function setBeaconIdToIndefiniteWhitelistStatus(
+        bytes32 _beaconId,
+        bool _indefiniteWhitelistStatus
+    ) external;
+
+    function whitelistReader(bytes32 _beaconId, address _reader) external;
+
+    function beaconIdToExpirationTimestamp(bytes32 _beaconId)
+        external
+        view
+        returns (uint64);
+
+    function beaconIdToIndefiniteWhitelistStatus(bytes32 _beaconId)
+        external
+        view
+        returns (bool);
+
+    function rrpBeaconServer()
+        external
+        view
+        returns (address);
+}
---
+++ contracts/oracle/Oracle.sol
@@ -0,0 +1,84 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.6.0 <=0.9.0;
+
+import "./IRrpBeaconServer.sol";
+import "./ISelfServeRrpBeaconServerWhitelister.sol";
+import "prb-math/contracts/PRBMathUD60x18.sol";
+
+contract Api3Oracle {
+    using PRBMathUD60x18 for uint256; 
+
+    /// @notice mapping of price feed key -> beacon id
+    mapping(bytes32 => bytes32) public priceFeedMap;
+
+    mapping(bytes32 => bool) public priceFeedWhitelisted;
+
+    uint256 SCALE = 10 ** 12;
+
+    IRrpBeaconServer RrpBeaconServer;
+    ISelfServeRrpBeaconServerWhitelister ssR;
+
+    event BeaconWhitelisted(bytes32 _priceFeedKey, bytes32 _beaconId);
+
+    constructor(address _whitelister) {
+        requireNonEmptyAddress(_whitelister);
+        ssR = ISelfServeRrpBeaconServerWhitelister(_whitelister);
+        RrpBeaconServer = IRrpBeaconServer(ssR.rrpBeaconServer());
+    }
+
+    /// @dev returns log(price) of an asset
+    /// @param _priceFeedKey price feed identifier/symbol of asset i.e ETH/USD
+    function getPrice(
+        bytes32 _priceFeedKey
+    ) external view returns(uint256){
+        require(priceFeedWhitelisted[_priceFeedKey], "PRICE_FEED: NOT WHITELISTED");
+
+        bytes32 beaconId = priceFeedMap[_priceFeedKey];
+        (int224 value, ) = RrpBeaconServer.readBeacon(beaconId);
+        uint256 scaledVal;
+        unchecked{
+           scaledVal = uint256(int256(value)) * SCALE ;
+        }
+        return scaledVal.log2();
+    }
+
+    // function getTwapPrice(
+    //     bytes32 _priceFeedKey,
+    //     uint256 _intervals
+    // ) external view returns(uint256){}
+
+    // function getPreviousPrice(
+    //     bytes32 _priceFeedKey,
+    //     uint256 at
+    // ) external view returns(uint256){}
+
+    /// @dev sets _beaconId to _priceFeedKey
+    /// @param _priceFeedKey price feed identifier/symbol of asset i.e ETH/USD
+    /// @param _beaconId beacon id 
+    function setBeacon(bytes32 _priceFeedKey, bytes32 _beaconId) external {
+        require(_beaconId != bytes32(0));
+       
+        if (!isPriceFeedWhitelisted(_priceFeedKey)){
+            ssR.whitelistReader(_beaconId, address(this));
+            priceFeedWhitelisted[_priceFeedKey] = true;
+        }
+        priceFeedMap[_priceFeedKey] = _beaconId;
+        
+        emit BeaconWhitelisted(_priceFeedKey, _beaconId);
+    }
+
+    /// @dev returns bool to indicate if price feed key is whitelisted or not
+    function isPriceFeedWhitelisted(bytes32 _priceFeedKey) public view returns(bool){
+        return priceFeedWhitelisted[_priceFeedKey];
+    }
+
+    /// @dev returns beacon id
+    function getBeacon(bytes32 _priceFeedKey) public view returns (bytes32) {
+        return priceFeedMap[_priceFeedKey];
+    }
+
+    function requireNonEmptyAddress(address _addr) internal pure {
+        require(_addr != address(0), "empty address");
+    }
+
+}
