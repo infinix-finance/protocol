8fb8e94a979eacd6c48be20b61b7ec21972ad592
Eric Lee <eric@advancedblockchain.com>
2022-04-18 17:03:11 -0400

    add e2e test for clearing house

--- contracts/mock/mocks/API3PriceFeedMock.sol
+++ contracts/mock/mocks/API3PriceFeedMock.sol
@@ -3,9 +3,11 @@ pragma solidity 0.6.9;
 
 contract API3PriceFeedMock {
     uint256 price;
+    uint256 twapPrice;
 
     constructor(uint256 _price) public {
         price = _price;
+        twapPrice = _price;
     }
 
     function getPrice(bytes32) public view returns (uint256) {
@@ -14,6 +16,14 @@ contract API3PriceFeedMock {
 
     function setPrice(uint256 _price) public {
         price = _price;
+    }
+
+    function getTwapPrice(bytes32, uint256) public view returns (uint256) {
+        return twapPrice;
+    }
+
+    function setTwapPrice(uint256 _price) public {
+        twapPrice = _price;
     }
 
     event PriceFeedDataSet(bytes32 key, uint256 price, uint256 timestamp, uint256 roundId);
